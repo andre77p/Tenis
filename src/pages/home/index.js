009867e8b84344c031c79d3968d28efa669cccb9
andre pereira <andre.p.rafael2018@gmail.com>
2022-09-21 17:21:41 -0300

    primeiro commit

---
+++ src/pages/home/index.js
@@ -0,0 +1,24 @@
+import './index.scss';
+
+
+export default function Index() {
+    return(
+        <main className='pagina-login'>
+            
+       
+        <h1 className='titulo'>Somente Administrador</h1>
+
+        <div className=''>
+            <h2>Login</h2>
+            <h2>Email</h2>
+            <input></input>
+            <h2>senha</h2>
+            <input></input>     
+        </div>
+
+      
+
+        
+        </main>
+    )
+}
